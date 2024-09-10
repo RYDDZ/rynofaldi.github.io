51d6ed7bbaa764372dfa52ca720790a9efe1f822
RYDDZ <162078445+RYDDZ@users.noreply.github.com>
2024-09-10 21:29:13 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+const sections = document.querySelectorAll(".slides");
+const navLinks = document.querySelectorAll(".navigation a");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    if (pageYOffset >= sectionTop - sectionHeight / 3) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach((link) => {
+    link.classList.remove("active");
+    if (link.getAttribute("href").includes(current)) {
+      link.classList.add("active");
+    }
+  });
+});
